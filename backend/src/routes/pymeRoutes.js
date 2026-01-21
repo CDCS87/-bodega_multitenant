24c469041e2489ad290222e2f2c61b52d62a59e7
CDCS87 <claudia.cabezas@alumno.iplacex.cl>
2026-01-21 16:23:25 -0300

    pymeruta

--- backend/src/routes/pymeRoutes.js
+++ backend/src/routes/pymeRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../database');
+const db = require('../config/database');
 const { authMiddleware: auth } = require('../middlewares/authMiddleware');
 
 // GET /api/pyme/me
